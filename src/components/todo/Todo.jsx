e8b8c4da5adc74cf335a8bd194e30edd4ecbd7fa
Yeonwoo Ga <vjk4874@naver.com>
2022-10-04 20:35:47 +0900

    최종 완성본

--- src/components/todo/Todo.jsx
+++ src/components/todo/Todo.jsx
@@ -3,7 +3,6 @@ import './style.css'
 
 
 function Todo(props){
-    if(props.item.isDone == false){
     return(  
                         <>
                         <div className="content-box">
@@ -13,28 +12,11 @@ function Todo(props){
                             </div>
                             <div className='btn-box'>
                                 <button id="del-btn" onClick={() => {props.deleteItem(props.item.id)}}>삭제하기</button>
-                                <button id="com-btn" onClick={() => {props.changeIsDone(props.item.id)}}>완료</button>
+                                <button id="com-btn" onClick={() => {props.changeIsDone(props.item.id)}}>{props.item.isDone == false ? '완료' : '취소'}</button>
                             </div>
                         </div>
                         </>
-
     )
-}else{
-    return(  
-        <>
-        <div className="content-box">
-            <div>
-                <h2>{props.item.title}</h2>
-                <div>{props.item.body}</div>
-            </div>
-            <div className='btn-box'>
-                <button id="del-btn" onClick={() => {props.deleteItem(props.item.id)}}>삭제하기</button>
-                <button id="com-btn" onClick={() => {props.changeIsDone(props.item.id)}}>취소</button>
-            </div>
-        </div>
-        </>
-    )
-}
 }
 
 
